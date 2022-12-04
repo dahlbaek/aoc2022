261e27286a69a48565ee9b7a943a83411b8e8f52
Jonas Dahlbæk <30782351+dahlbaek@users.noreply.github.com>
2022-12-04 18:26:12 +0100

    Day 2: Clean up deserialization

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,26 +1,15 @@
-struct Symbol(i8);
-
-impl From<char> for Symbol {
-    fn from(c: char) -> Symbol {
-        match c {
-            'A' | 'X' => Symbol(-1),
-            'B' | 'Y' => Symbol(0),
-            'C' | 'Z' => Symbol(1),
-            _ => unreachable!(),
-        }
+fn read(b: u8) -> i8 {
+    match b {
+        b'A' | b'X' => -1,
+        b'B' | b'Y' => 0,
+        b'C' | b'Z' => 1,
+        _ => panic!("Found unrecognized byte {}", b),
     }
 }
 
-struct Input {
-    their_choice: Symbol,
-    unknown: Symbol,
-}
-
-fn from_str(s: &str) -> Input {
-    Input {
-        their_choice: Symbol::from(s.chars().next().unwrap()),
-        unknown: Symbol::from(s.chars().nth(2).unwrap()),
-    }
+fn prepare(s: &str) -> (i8, i8) {
+    let mut bytes = s.bytes();
+    (read(bytes.next().unwrap()), read(bytes.nth(1).unwrap()))
 }
 
 fn transform(i: i8) -> i8 {
@@ -31,35 +20,35 @@ fn transform(i: i8) -> i8 {
     }
 }
 
-fn choice(input: Input) -> u64 {
-    let outcome = transform(input.unknown.0 - input.their_choice.0);
-    (input.unknown.0 + 2) as u64 + 3 * (outcome + 1) as u64
+fn choice((their_choice, choice): (i8, i8)) -> u64 {
+    let outcome = transform(choice - their_choice);
+    (choice + 2) as u64 + 3 * (outcome + 1) as u64
 }
 
-fn outcome(input: Input) -> u64 {
-    let choice = transform(input.unknown.0 + input.their_choice.0);
-    (choice + 2) as u64 + 3 * (input.unknown.0 + 1) as u64
+fn outcome((their_choice, outcome): (i8, i8)) -> u64 {
+    let choice = transform(outcome + their_choice);
+    (choice + 2) as u64 + 3 * (outcome + 1) as u64
 }
 
 fn main() {
     let input = include_str!("input");
-    println!("{}", input.lines().map(from_str).map(choice).sum::<u64>());
-    println!("{}", input.lines().map(from_str).map(outcome).sum::<u64>());
+    println!("{}", input.lines().map(prepare).map(choice).sum::<u64>());
+    println!("{}", input.lines().map(prepare).map(outcome).sum::<u64>());
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{choice, from_str, outcome};
+    use crate::{choice, outcome, prepare};
 
     const INPUT: &'static str = include_str!("test-input");
 
     #[test]
     fn it_works_1() {
-        assert_eq!(INPUT.lines().map(from_str).map(choice).sum::<u64>(), 15);
+        assert_eq!(INPUT.lines().map(prepare).map(choice).sum::<u64>(), 15);
     }
 
     #[test]
     fn it_works_2() {
-        assert_eq!(INPUT.lines().map(from_str).map(outcome).sum::<u64>(), 12);
+        assert_eq!(INPUT.lines().map(prepare).map(outcome).sum::<u64>(), 12);
     }
 }
